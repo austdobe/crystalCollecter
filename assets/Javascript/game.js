96d64ee3bc104ae6fa6616b2c235869adc4836dc
Austin <austin.dober@gmail.com>
2019-07-20 16:01:38 -0400

    finalized working game

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,5 +1,6 @@
+//declaring global variables
 var randNum = "";
-var score = 0;
+var score;
 var cryst1="";
 var cryst2="";
 var cryst3="";
@@ -10,45 +11,44 @@ var lossCounter=0;
 function startGame(){
     // reset game
     score = 0;
-   randNum = Math.floor(Math.random()* 120) + 19;
-   console.log(randNum);
-   cryst1 = Math.floor(Math.random()* 12) + 1;
-   cryst2 = Math.floor(Math.random()* 12) + 1;
-   cryst3 = Math.floor(Math.random()* 12) + 1;
-   cryst4 = Math.floor(Math.random()* 12) + 1;
-    
+    //Random Number calculation
+    randNum = Math.floor(Math.random()* 120) + 19;
+    console.log(randNum);
+    //crystal calculation
+    cryst1 = Math.floor(Math.random()* 12) + 1;
+    cryst2 = Math.floor(Math.random()* 12) + 1;
+    cryst3 = Math.floor(Math.random()* 12) + 1;
+    cryst4 = Math.floor(Math.random()* 12) + 1;
+    // DOM Manipulation    
     document.getElementById("randomNum").innerHTML = randNum;
     document.getElementById("score").innerHTML = score;
     document.getElementById("win").innerHTML=winCounter;
     document.getElementById("loses").innerHTML = lossCounter;
-    console.log(" This is 1: " + cryst1)
-    console.log(" This is 2: " + cryst2)
-    console.log(" This is 3: " + cryst3)
-    console.log(" This is 4: " + cryst4)
-
-    $("#crystal1").click(function(){
-        score += cryst1;
-        document.getElementById("score").innerHTML=score;
-        checkscore();
-    });
-    $("#crystal2").click(function(){
-        score += cryst2;
-        document.getElementById("score").innerHTML = score;
-        checkscore();
-    });
-    $("#crystal3").click(function(){
-        score += cryst3;
-        document.getElementById("score").innerHTML = score;
-        checkscore();
-    });
-    $("#crystal4").click(function(){
-        score += cryst4;
-        document.getElementById("score").innerHTML = score;
-        checkscore();
-    });
 }
+//on image click add appropriate amount based on above calclations
+$("#crystal1").click(function(){
+    score += cryst1;
+    document.getElementById("score").innerHTML = score;
+    checkscore();
+});
+$("#crystal2").click(function(){
+    score += cryst2;
+    document.getElementById("score").innerHTML = score;
+    checkscore();
+});
+$("#crystal3").click(function(){
+    score += cryst3;
+    document.getElementById("score").innerHTML = score;
+    checkscore();
+});
+$("#crystal4").click(function(){
+    score += cryst4;
+    document.getElementById("score").innerHTML = score;
+    checkscore();
+});
+//function to check if won or not. 
 function checkscore(){
-    if(score==randNum){
+    if(score===randNum){
         winCounter++;
         alert("You win!")
         document.getElementById("win").innerHTML=winCounter;
@@ -57,9 +57,10 @@ function checkscore(){
         lossCounter++;
         alert("Try again!")
         document.getElementById("loses").innerHTML=lossCounter;
-        startGame()
+        startGame();
     };
 }
+// initial start
 startGame();
 
 
